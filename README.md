6e83079676f369691d1d0aa77ba9bf71de2beec8
Sameer Manek <sameer.manek@gmail.com>
2016-05-09 11:15:47 -0400

    Added content
    Added the chrome extension

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # TimeToSmile
+----
+Automatically redirect to smile.amazon.com when visiting an amazon product website and add my amazon associate tag when no other tag exists
+
---
+++ chrome/action.js
@@ -0,0 +1,59 @@
+chrome.webRequest.onBeforeRequest.addListener(function(details) {
+    var url = details.url;
+
+    // First, detect whether this is an associate url
+    var associateFilter = "tag=";
+
+    var myAssociateTag = "tag=sm11235813-20";
+
+    // Exclusion Filter (borrowed from smilealways)
+      var exclusionFilter = "(timetosmile-no-redirect=1)"
+                      + "|(redirect=true)"
+                      + "|(redirect.html)"
+                      + "|(r.html)"
+                      + "|(/gp/dmusic/cloudplayer)"
+                      + "|(/gp/photos)"
+                      + "|(/gp/wishlist)"
+                      + "|(/ap/)"
+                      + "|(amazon.com/clouddrive)";
+
+    //  Don't tough
+    if (url.match(exclusionFilter)) {
+      return;
+    }
+
+    // build url
+    var newUrl;
+    // First, add in the smile component
+    newUrl = url.replace(/\/(www\.)?amazon\.com/i, "smile.amazon.com");
+
+
+    // Add on my tag.
+    if(!newUrl.match(associateFilter)) {
+      // May need to be more strict -- how do they escape things in the title?
+      // A few examples seem to drop them, so should be ok. 
+      if(newUrl.match(/\?/i) != null) {
+        newUrl = newUrl + "&" + myAssociateTag;
+      } else {
+        newUrl = newUrl + "?" + myAssociateTag;
+      }
+    }
+
+    // Add a no-redirect
+    newUrl = newUrl + "&timetosmile-no-redirect=1";
+
+        return {
+          redirectUrl : newUrl
+        }
+    return;
+}, {
+    urls : ["http://amazon.com/*",
+            "https://amazon.com/*",
+            "http://www.amazon.com/*", 
+            "https://www.amazon.com/*",
+            "http://smile.amazon.com/*",
+            "https://smile.amazon.com/*"
+            ],
+    types: ["main_frame","sub_frame"]
+}, ["blocking"]);
+
